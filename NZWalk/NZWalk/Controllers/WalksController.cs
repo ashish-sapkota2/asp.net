20cb8d9d5c55a4276a84e7cf0a89bdab1df97366
Ashish Sapkota <aryanashish223@gmail.com>
2024-06-17 22:23:56 +0545

    filtering sorting and paging

--- NZWalk/NZWalk/Controllers/WalksController.cs
+++ NZWalk/NZWalk/Controllers/WalksController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalk.CustomActionFilters;
 using NZWalk.Models;
 using NZWalk.Models.DTO;
 using NZWalk.Repositories;
@@ -22,23 +23,29 @@ namespace NZWalk.Controllers
         //create walk
         //POST: //api/walks
         [HttpPost]
+        [validateModel]
         public async Task<IActionResult> Create([FromBody] AddWalkRequestDto addWalkRequestDto)
         {
-            // map dto to domain model
-            var walkdomainmodel=  maper.Map<Walk>(addWalkRequestDto);
-            await walkRepository.CreateAsync(walkdomainmodel);  
 
-            //map domain model to dto
+                // map dto to domain model
+                var walkdomainmodel = maper.Map<Walk>(addWalkRequestDto);
+                await walkRepository.CreateAsync(walkdomainmodel);
+
+                //map domain model to dto
 
 
-            return Ok(maper.Map<WalkDto>(walkdomainmodel));
+                return Ok(maper.Map<WalkDto>(walkdomainmodel));
+           
         }
         //GET Walks
+        //GET: /api/walks?filterOn=Name&filterQuery=TracksortBy=Name&isAscending=true
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery]string? filterOn, [FromQuery] string? filterQuery,
+            [FromQuery] string? sortBy, [FromQuery] bool? isAscending
+            )
         {
-            var walkdomainmodel=await walkRepository.GetAllAsync();
+            var walkdomainmodel=await walkRepository.GetAllAsync(filterOn,filterQuery, sortBy, isAscending?? true);
 
             //map domain model to dto
 
@@ -62,21 +69,23 @@ namespace NZWalk.Controllers
 
         //PUT walk
         [HttpPut("{id:guid}")]
+        [validateModel]
 
         public async Task<IActionResult> Update(Guid id,UpdateWalkRequestDto updateWalkRequestDto) 
         {
-            //map DTO to domain model
-            var walkDomainmodel = maper.Map<Walk>(updateWalkRequestDto);
-            await walkRepository.UpdateAsync(id, walkDomainmodel);
+                //map DTO to domain model
+                var walkDomainmodel = maper.Map<Walk>(updateWalkRequestDto);
+                await walkRepository.UpdateAsync(id, walkDomainmodel);
 
-            if (walkDomainmodel == null)
-            {
-                return NotFound();
-            }
-            //map domain model to DTO
+                if (walkDomainmodel == null)
+                {
+                    return NotFound();
+                }
+                //map domain model to DTO
 
-            return Ok(maper.Map<WalkDto>(walkDomainmodel));
-        }
+                return Ok(maper.Map<WalkDto>(walkDomainmodel));
+            
+         }
 
         //Delete Walk
         [HttpDelete("{id:guid}")]
--- NZWalk/NZWalk/Models/DTO/AddWalkRequestDto.cs
+++ NZWalk/NZWalk/Models/DTO/AddWalkRequestDto.cs
@@ -1,12 +1,25 @@
-﻿namespace NZWalk.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalk.Models.DTO
 {
     public class AddWalkRequestDto
     {
+        [Required]
+        [MaxLength(100)]
+
         public string Name { get; set; }
+        [Required]
+        [MaxLength(1000)]
+
         public string Description { get; set; }
+        [Required]
+        [Range(0,50)]
         public double LengthInKm { get; set; }
+      
         public string? WalkImageUrl { get; set; }
+        [Required]
         public Guid DifficultyId { get; set; }
+        [Required]
         public Guid RegionId { get; set; }
     }
 }
