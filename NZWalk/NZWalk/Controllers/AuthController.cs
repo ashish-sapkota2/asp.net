693a16aae71bef4e63aa7bad2f81e358df7d5850
Ashish Sapkota <aryanashish223@gmail.com>
2024-06-18 22:59:11 +0545

    Authorization and authentication

---
+++ NZWalk/NZWalk/Controllers/AuthController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalk.Models.DTO;
+using NZWalk.Repositories;
+
+namespace NZWalk.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly ITokenRepository tokenRepository;
+
+        public AuthController(UserManager<IdentityUser> userManager, ITokenRepository tokenRepository)
+        {
+            this.userManager = userManager;
+            this.tokenRepository = tokenRepository;
+        }
+
+        //POST:/api/Auth/Register
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username,
+            };
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDto.Password);
+            if (identityResult.Succeeded)
+            {
+                // Add roles to this user
+                if (registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User was Registered");
+                    }
+
+                }
+            }
+            return BadRequest("Something went wrong");
+        }
+        //POST: /api/Auth/login
+
+        [HttpPost]
+        [Route("Login")]
+
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        {
+            var user = await userManager.FindByEmailAsync(loginRequestDto.Username);
+
+            if (user != null)
+            {
+                var checkPasswordResult = await userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+                if (checkPasswordResult)
+                {
+                    //get the roles for user
+
+                    var roles = await userManager.GetRolesAsync(user);
+                    if (roles != null) {
+
+
+                        //create token
+                       var jwtToken= tokenRepository.CreateJWTToken(user, roles.ToList());
+
+                        var response = new LoginResponseDto
+                        {
+                            JwtToken = jwtToken
+                        };
+                        
+                        return Ok(response);
+                    }
+                   
+                }
+            }
+            return BadRequest("username or pw incorrect");
+
+
+        }
+    }
+}
---
+++ NZWalk/NZWalk/Models/DTO/LoginResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace NZWalk.Models.DTO
+{
+    public class LoginResponseDto
+    {
+        public string JwtToken { get; set; }
+    }
+}
