09a5b1654285500757cc0b0345efc172d2a61735
Ashish Sapkota <aryanashish223@gmail.com>
2024-06-22 21:45:10 +0545

    dating app started

---
+++ DatingApp/DatingApp/client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'The Dating App';
+  user: any;
+
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.user = response;
+
+    }, error => {
+      console.log(error);
+    })
+  }
+}
