dcb9823767ccd1b1fcb3fe44ba62dab60a50103f
Ashish Sapkota <aryanashish223@gmail.com>
2024-07-03 21:31:47 +0545

    Contactly

---
+++ Contactly/Contactly/Models/AddRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Contactly.Models
+{
+    public class AddRequestDto
+    {
+        public required string Name { get; set; }
+        public string? Email { get; set; }
+        public required string Phone { get; set; }
+        public bool Favourite { get; set; }
+    }
+}
