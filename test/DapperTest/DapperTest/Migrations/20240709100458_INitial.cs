7dd1d04cca6afa99aa7e32dc73d581591bd0f352
unknown <aryanashish223@gmail.com>
2024-07-09 17:27:19 +0545

    dapper

---
+++ test/DapperTest/DapperTest/Migrations/20240709100458_INitial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DapperTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class INitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "toDo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Stauts = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_toDo", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "toDo");
+        }
+    }
+}
--- test/test/API/Controllers/AccountController.cs
+++ test/test/API/Controllers/AccountController.cs
@@ -34,7 +34,7 @@ namespace Test.API.Controllers
                 using var hmac = new HMACSHA512();
                 var user = new AppUser
                 {
-                    UserName = registerDto.Username,
+                    UserName = registerDto.Username.ToLower(),
                     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                     PasswordSalt =hmac.Key
                 };
