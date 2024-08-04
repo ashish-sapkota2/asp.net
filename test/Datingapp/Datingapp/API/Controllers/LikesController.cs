aa9bf863626397081ae292061061c761d7bc55fd
Ashish Sapkota <aryanashish223@gmail.com>
2024-08-04 20:52:40 +0545

    like

---
+++ test/Datingapp/Datingapp/API/Controllers/LikesController.cs
@@ -0,0 +1,56 @@
+﻿using Datingapp.API.DTO;
+using Datingapp.API.Extensions;
+using Datingapp.API.Interface;
+using Datingapp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Datingapp.API.Controllers
+{
+    [Authorize]
+    public class LikesController: BaseApiController
+    {
+        private readonly IUserRepository userRepository;
+        private readonly ILikesRepository likesRepository;
+
+        public LikesController(IUserRepository userRepository, ILikesRepository likesRepository)
+        {
+            this.userRepository = userRepository;
+            this.likesRepository = likesRepository; 
+        }
+        [HttpPost("{username}")]
+        public async Task<ActionResult>AddLike(string username)
+        {
+            var sourceUserId = User.GetUserId();
+            var likedUser = await userRepository.GetByUsername(username);
+            var sourceUser = await likesRepository.GetUserWithLikes(sourceUserId);
+
+            if (likedUser == null) return NotFound();
+
+            if (sourceUser.UserName == username) return BadRequest("You cannot like yourself");
+
+            var userLike = await likesRepository.GetUserLike(sourceUserId, likedUser.Id);
+
+            if (userLike != null) return BadRequest("You already like this user");
+
+            userLike = new UserLike
+            {
+                SourceUserId = sourceUserId,
+                LikedUserId = likedUser.Id,
+            };
+            
+            sourceUser.LikedUsers.Add(userLike);
+
+            if(await userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("failed to like user");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes(string predicate)
+        {
+            var users = await likesRepository.GetUserLikes(predicate, User.GetUserId());
+            return Ok(users);
+        }
+    }
+}
--- test/Datingapp/Datingapp/API/Data/LikesRepository.cs
+++ test/Datingapp/Datingapp/API/Data/LikesRepository.cs
@@ -1,26 +1,57 @@
 ﻿using Datingapp.API.DTO;
+using Datingapp.API.Extensions;
 using Datingapp.API.Interface;
 using Datingapp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Datingapp.API.Data
 {
     public class LikesRepository : ILikesRepository
        
     {
-        public LikesRepository(DataContext context) { }
-        public Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
+        private readonly DataContext context;
+
+        public LikesRepository(DataContext context)
         {
-            throw new NotImplementedException();
+            this.context = context;
+        }
+        public async Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
+        {
+            return await context.Likes.FindAsync(sourceUserId, likedUserId);
+
         }
 
-        public Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+        public async Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
         {
-            throw new NotImplementedException();
+            var users = context.Users.OrderBy(u=>u.UserName).AsQueryable();
+            var likes = context.Likes.AsQueryable();
+            if (predicate == "liked")
+            {
+                likes =likes.Where(like=>like.SourceUserId==userId);
+                users= likes.Select(like=>like.LikedUser); 
+            }
+            if(predicate == "likedBy")
+            {
+                likes = likes.Where(like => like.LikedUserId == userId);
+                users = likes.Select(like => like.SourceUser);
+            }
+            return await users.Select(user=>new LikeDto
+            {
+                Username = user.UserName,
+                KnownAs= user.KnownAs,
+                Age= user.DateOfBirth.CalculateAge(),
+                PhotoUrl =user.Photos.FirstOrDefault(p=>p.IsMain).Url,
+                City = user.City,
+                Id = user.Id
+
+            }).ToListAsync();
         }
 
-        public Task<AppUser> GetUserWithLikes(int userId)
+        public async Task<AppUser> GetUserWithLikes(int userId)
         {
-            throw new NotImplementedException();
+            return await context.Users
+                .Include(x => x.LikedUsers)
+                .FirstOrDefaultAsync(x => x.Id == userId);
         }
     }
 }
